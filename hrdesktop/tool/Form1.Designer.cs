0fe6847a785850604a691060f2b9a648d35737ff
tei952 <tei952@hotmail.com>
2017-03-25 14:53:44 +0900

    add tool to load job type
    add tool to load job type

---
+++ hrdesktop/tool/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace highwayns
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trvJobType = new System.Windows.Forms.TreeView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // trvJobType
+            // 
+            this.trvJobType.Location = new System.Drawing.Point(76, 22);
+            this.trvJobType.Name = "trvJobType";
+            this.trvJobType.Size = new System.Drawing.Size(361, 225);
+            this.trvJobType.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(76, 254);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(115, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Job Type";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 295);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.trvJobType);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView trvJobType;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
