2b8cba446e77e7fe695b0dc14c917aa625c175ff
refinement <931523144@qq.com>
2017-07-26 20:05:07 +0800

    add

---
+++ hrapp/android/app/src/main/java/com/highwayns/ht/ui/activity/AboutBasicInfoActivity.java
@@ -0,0 +1,14 @@
+package com.highwayns.ht.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.highwayns.ht.R;
+
+public class AboutBasicInfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_info_aboutus);
+    }
+}
---
+++ hrapp/android/app/src/main/java/com/highwayns/ht/ui/activity/EnterpriseBusinessActivity.java
@@ -0,0 +1,14 @@
+package com.highwayns.ht.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.highwayns.ht.R;
+
+public class EnterpriseBusinessActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_enterprise);
+    }
+}
